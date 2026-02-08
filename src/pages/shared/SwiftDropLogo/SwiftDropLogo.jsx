65094842ce97173766fbf2125df77931963fe97c
Shams Saniat <shams.saniat@northsouth.edu>
2026-02-08 01:59:27 +0000

    Root layout and Navbar setup

---
+++ src/pages/shared/SwiftDropLogo/SwiftDropLogo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../../assets/logo.png'
+
+const SwiftDropLogo = () => {
+    return (
+        <div>
+            <img src={logo} alt="" />
+            <p>Profast</p>
+        </div>
+    );
+};
+
+export default SwiftDropLogo;
