fe91e5437f096dea0adc4d7e7567fb78f32db4f4
Shams Saniat <shams.saniat@northsouth.edu>
2026-06-14 01:37:32 +0100

    Display map and services area marker using react Leaflet, added search location to fly

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import logo from "../../assets/logo.png"
+import { Link } from 'react-router';
 
 const Logo = () => {
     return (
-        <div className='flex items-end'>
-            <img src={logo} alt="" />
-            <h3 className="3xl font-bold -ms-2.5">SwiftDropBD</h3>
-        </div>
+        <Link to="/">
+            <div className='flex items-end'>
+                <img src={logo} alt="" />
+                <h3 className="3xl font-bold -ms-2.5">SwiftDropBD</h3>
+            </div>
+        </Link>
     );
 };
 
